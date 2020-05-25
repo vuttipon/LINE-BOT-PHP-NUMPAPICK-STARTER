4186c25c4ac1bea72d1a2093dcbfc6ace01dc5a9
vuttipon <65884765+vuttipon@users.noreply.github.com>
2020-05-25 14:41:46 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'FL+/K9TNqKpyQcnLwg/tcYMeT25mZH1wBWWzEMGpJBIae/iV98Cm2G2axpXn33d4G2siW60ij5c6YfW0JX/kNSgwQAau+DvLQBpELEoTmup3eg6RvRhNQkN4WUwkbwlXCKhyWv0PALIYzHAxiy3ddAdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => '9dfd886e97641ac484437f7fcb6ed5d0',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
