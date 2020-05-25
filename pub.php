731c4ba9084ac027208136ad10889ab09d420587
vuttipon <65884765+vuttipon@users.noreply.github.com>
2020-05-25 14:37:22 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -1,8 +1,8 @@
  <?php
  function pubMqtt($topic,$msg){
-       $APPID= "numpapicklinebot/"; //enter your appid
-     $KEY = "mJ7K4MfteC7p0dW"; //enter your key
-    $SECRET = "pp4gzMhCvJIqlxc66hKEvk46m"; //enter your secret
+       $APPID= "vuttiponlinebot/"; //enter your appid
+     $KEY = "kN8GFIglPiC4luL"; //enter your key
+    $SECRET = "UkOe6VXhXNlyZq887r7YMJS0X"; //enter your secret
     $Topic = "$topic"; 
       put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
  
